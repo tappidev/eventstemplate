fba779819dcbc8384b03dd3ecb6523d6a1532dc1
Colin <cmfadden@tappi.org>
2015-09-17 17:28:54 -0400

    Added Nano dependencies/content

--- App/gulpfile.js
+++ App/gulpfile.js
@@ -41,6 +41,17 @@ gulp.task('supercorr-sass', function () {
         .pipe(gulp.dest('../css/'));
 });
 
+gulp.task('nano-sass', function () {
+    gulp.src('scss/nano.scss')
+        //.pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        //.pipe(sourcemaps.write())
+        .pipe(concat('nanotechnology-style.css'))
+        .pipe(gulp.dest('../css/'));
+});
+
 // watch tasks
 
 gulp.task('watch', function () {
@@ -57,8 +68,20 @@ gulp.task('supercorr-watch', function () {
     gulp.watch('scss/**/*.scss', ['supercorr-sass'])
 });
 
+// nano watch task
+gulp.task('nano-watch', function () {
+    gulp.watch('scss/**/*.scss', ['nano-sass'])
+});
+
+// Combine all tasks
+gulp.task('all', ['sass', 'papercon-sass', 'supercorr-sass', 'nano-sass']);
+gulp.task('watch-all', ['watch', 'papercon-watch', 'supercorr-watch', 'nano-watch']);
+
 // create default task
 
-gulp.task('default', ['sass', 'watch']);
+/*gulp.task('default', ['sass', 'watch']);
 gulp.task('papercon', ['papercon-sass', 'papercon-watch']);
 gulp.task('supercorr', ['supercorr-sass', 'supercorr-watch']);
+gulp.task('nano', ['nano-sass', 'nano-watch']);*/
+
+gulp.task('default', ['all', 'watch-all']);
